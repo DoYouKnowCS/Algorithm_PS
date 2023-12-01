0038cac27791f84d0f17b3359b923bfc72ae5a5d
ssangdon <tngustkdehs@naver.com>
2023-12-01 16:47:06 +0900

    [success]boj_에너지모으기기\|기본적인 백트래킹

---
+++ Sangdon/Week2/boj_16198_에너지모으기.js
@@ -0,0 +1,26 @@
+let inputs = require("fs")
+  .readFileSync("boj_16198_에너지모으기.txt")
+  .toString()
+  .trim()
+  .split("\n");
+function solution(input) {
+  let n = +input[0];
+  let arrs = input[1].split(" ").map(d => +d);
+  let total = 0;
+  const re = (arr, sum) => {
+    if (arr.length < 3) {
+      total = Math.max(total, sum);
+      return 0;
+    }
+    for (var i = 1; i < arr.length - 1; i++) {
+      let count = arr[i - 1] * arr[i + 1];
+      sum += count;
+      let rest = [...arr.slice(0, i), ...arr.slice(i + 1)];
+      re(rest, sum);
+      sum -= count;
+    }
+  };
+  re(arrs, 0);
+  console.log(total);
+}
+solution(inputs);
