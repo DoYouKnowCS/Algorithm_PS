019a78a98f0cc5cfd858c36b18c26efc6931f7fd
Jihwan Lee <wlghks0508@naver.com>
2023-12-02 21:58:44 +0900

    Create boj_16198_에너지 모으기.java
    [success]: boj_16198_에너지 모으기|linkedlist를 사용해서 제거, 추가를 간단하게 했다.

---
+++ takeitEasyhwan/Week2/boj_16198_에너지 모으기.java
@@ -0,0 +1,34 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int maxSum = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        for(int i=0;i<N;i++) {
+            linkedList.add(Integer.parseInt(st.nextToken()));
+        }
+        req(0,linkedList);
+        System.out.println(maxSum);
+    }
+    private static void req(int tempSum, LinkedList<Integer> linkedList) {
+        if(linkedList.size()==2) {
+            maxSum = Math.max(maxSum,tempSum);
+            return;
+        }
+        for(int i=1;i<linkedList.size()-1;i++) {
+            int exceptNum = linkedList.get(i);
+            linkedList.remove(i);
+            req(linkedList.get(i-1) * linkedList.get(i) + tempSum,linkedList);
+            linkedList.add(i,exceptNum);
+        }
+    }
+}
