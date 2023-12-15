269cad8185126fe9078bf09bfd1fed641b7f5d27
ssangdon <tngustkdehs@naver.com>
2023-12-15 14:52:11 +0900

    [success] : boj_28070_유니의 편지쓰기| 시간복잡도를 위한 누적합 문제 | 프로그래머스 파괴되지 않은 건물과 유사한 문제라 생각하고 풀었음

---
+++ Sangdon/Week4/boj_28070_유니의 편지쓰기.js
@@ -0,0 +1,47 @@
+let inputs = require("fs")
+  .readFileSync("boj_28070_유니의 편지쓰기.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+function solution(input) {
+  let n = +input[0];
+  let max = 0;
+  let idx = 0;
+  // 200001 -> 0
+  // 200002 -> 1
+  // 200009 -> 8
+  // 200012 -> 11
+  // 200101 -> 12
+  // 200102 -> 13
+  // 999912
+  // 7999 * 12
+  let arr = new Array(100000).fill(0);
+  let sum = new Array(100000).fill(0);
+  sum[0] = arr[0];
+  for (var i = 1; i < 1 + n; i++) {
+    let [start, end] = input[i].split(" ");
+    let [sY, sD] = start.split("-").map(Number);
+    let [eY, eD] = end.split("-").map(Number);
+    let sI = (sY - 2000) * 12 + sD - 1;
+    let eI = (eY - 2000) * 12 + eD - 1;
+    arr[sI] += 1;
+    arr[eI + 1] -= 1;
+  }
+  for (var i = 1; i < sum.length; i++) {
+    sum[i] = arr[i] + sum[i - 1];
+    if (sum[i] > max) {
+      max = sum[i];
+      idx = i;
+    }
+  }
+  //   console.log(idx);
+  let month = 2000 + Math.floor(idx / 12);
+  let day = (idx % 12) + 1;
+  if (Math.floor(day / 10) > 0) {
+    console.log(`${month}-${day}`);
+  } else {
+    console.log(`${month}-0${day}`);
+  }
+}
+solution(inputs);
