c872c538e4dd8f4a11a5b35eca92d5fd95860503
Jihwan Lee <wlghks0508@naver.com>
2023-12-27 15:06:46 +0900

    [fail]:boj_18310_안테나 | 구현하기는 매우 쉬우나 패턴을 찾기 어렵다. 평균 값이 제일 효율적이라고 생각했는데 아니었다.

---
+++ takeitEasyhwan/Week6/boj_18310_안테나.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int[] arr = new int[N];
+        for(int i=0;i<N;i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+        System.out.println(N%2==0?arr[N/2-1]:arr[N/2]);
+    }
+}
