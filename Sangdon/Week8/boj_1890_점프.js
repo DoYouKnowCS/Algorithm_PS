47b2a7745c16ddddb9b09347c6a59e6ce2a365df
ssangdon <tngustkdehs@naver.com>
2024-01-23 21:09:19 +0900

    [fail]: boj_1890_점프| 2차원배열을 이용한 DP문제

---
+++ Sangdon/Week8/boj_1890_점프.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const filePath =
+  process.platform === "linux" ? "/dev/stdin" : "boj_1890_점프.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+let n = Number(input[0]);
+let arr = new Array(n).fill(new Array(n));
+for (let i = 0; i < arr.length; i++) {
+  arr[i] = input[i + 1].split(" ").map(Number);
+}
+let dp = new Array(n);
+for (let i = 0; i < dp.length; i++) {
+  dp[i] = new Array(n).fill(BigInt(0));
+}
+dp[0][0] = BigInt(1);
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < n; j++) {
+    let num = arr[i][j];
+    if (num === 0) continue;
+    if (i + arr[i][j] < n) {
+      dp[i + arr[i][j]][j] += dp[i][j];
+    }
+    if (j + arr[i][j] < n) {
+      dp[i][j + arr[i][j]] += dp[i][j];
+    }
+  }
+}
+console.log(dp[n - 1][n - 1].toString());
