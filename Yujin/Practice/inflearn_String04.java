18eea5186c5a44715236a0cd1c62204cb262c867
uujiinn <jane9994@gmail.com>
2024-02-22 23:56:30 +0900

    [success] : inflearn_String04 | 문자열 뒤집기

---
+++ Yujin/Practice/inflearn_String04.java
@@ -0,0 +1,23 @@
+package inflearn;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class inflearn_01_04 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        String[] str = new String[n];
+        for(int i=0; i<n; i++){
+            str[i]=scan.next();
+        }
+        ArrayList<String> answer = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            String tmp = new StringBuilder()
+        }
+
+
+        System.out.println(answer);
+
+    }
+}
