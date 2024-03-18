699044ef7e8cfb75b12478d751b16a19f720c997
uujiinn <irislxxyujin@gmail.com>
2024-03-18 23:29:22 +0900

    [success] : inflearn_2Pointers05 | 연속된 자연수의 합
    int lt=0;
    for(int rt=0; rt<num; rt++){
        sum +=arr[rt];
        if(sum==n) answer++;
        while(sum>=n){
            sum-=arr[lt++];
            if(sum==n) answer++;
        }
    }

---
+++ Yujin/Practice/inflearn_2Pointers05.java
@@ -0,0 +1,30 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class inflearn_03_05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int answer = 0, sum=0;
+        int num = n/2 + 1;
+
+        int[] arr = new int[num];
+        for(int i=0; i<num ; i++){
+            arr[i] = i+1;
+        }
+        int lt=0;
+
+        for(int rt=0; rt<num; rt++){
+            sum +=arr[rt];
+            if(sum==n) answer++;
+            while(sum>=n){
+                sum-=arr[lt++];
+                if(sum==n) answer++;
+            }
+        }
+
+        System.out.println(answer);
+
+    }
+}
