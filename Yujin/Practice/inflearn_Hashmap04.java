65e25b01b1361ee10cca059cd98478f0da461ad8
uujiinn <irislxxyujin@gmail.com>
2024-03-21 23:13:35 +0900

    [success] : inflearn_Hashmap04 | 모든 아나그램 찾기

--- Yujin/Practice/inflearn_Hashmap04.java
+++ Yujin/Practice/inflearn_Hashmap04.java
@@ -3,7 +3,7 @@ package inflearn;
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class inflearn_04_01 {
+public class inflearn_04_04 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String S = sc.next();
