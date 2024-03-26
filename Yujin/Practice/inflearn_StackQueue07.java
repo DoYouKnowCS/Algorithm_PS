931e8fb3a693e9ee285b26a4e4827425ed0c9eab
uujiinn <irislxxyujin@gmail.com>
2024-03-26 23:17:53 +0900

    Create inflearn_StackQueue07.java

---
+++ Yujin/Practice/inflearn_StackQueue07.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+  
+public class Main {
+  public static void main(String[] args){
+    Scanner in=new Scanner(System.in);
+
+    //교육과정설계
+    int input1 = in.nextInt();
+    int input2 = in.nextInt();
+    System.out.println(input1 + input2);
+    return ;
+  }
+}
