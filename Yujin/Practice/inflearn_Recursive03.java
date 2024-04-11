2b0467a87058fac1627309a2a2808ea947e6c1a5
uujiinn <irislxxyujin@gmail.com>
2024-04-11 23:16:39 +0900

    [success] : inflearn_Recursive03 | 재귀함수_팩토리얼

---
+++ Yujin/Practice/inflearn_Recursive03.java
@@ -0,0 +1,19 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class inflearn_07_03 {
+    public int DFS(int n){
+        if(n==1) return 1;
+        else{
+            return n*DFS(n-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        inflearn_07_03 T = new inflearn_07_03();
+        System.out.println(T.DFS(n));
+    }
+}
