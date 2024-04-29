65de5e2f59562d567810edaf0779bff7fea0ea2d
uujiinn <irislxxyujin@gmail.com>
2024-04-29 23:54:31 +0900

    [success] : inflearn_Recursive14 | 그래프 최단거리 BFS 인접리스트(ArrayList)

--- Yujin/Practice/inflearn_Recursive14.java
+++ Yujin/Practice/inflearn_Recursive14.java
@@ -1,1 +1,48 @@
-//수정중
+package inflearn;
+
+import java.util.*;
+
+public class inflearn_07_14 {
+    static int n, m;
+    static ArrayList<ArrayList<Integer>> graph; //ArrayList를 갖는 ArrayList
+    static int[] ch, dis;
+
+    public void BFS(int v){
+        Queue<Integer> queue = new LinkedList<>();
+        ch[v]=1;
+        dis[v]=0;
+        queue.offer(v);
+        while(!queue.isEmpty()){
+            int cv = queue.poll();
+            for(int nv : graph.get(cv)){
+                if(ch[nv]==0){
+                    ch[nv]=1;
+                    queue.offer(nv);
+                    dis[nv]=dis[cv]+1; //다음은 현재 +1
+                }
+            }
+        }             
+    }
+
+    public static void main(String[] args) {
+        inflearn_07_14 T = new inflearn_07_14();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt(); //정점 수
+        m = sc.nextInt(); //간선 수
+        graph = new ArrayList<ArrayList<Integer>>(); //인접리스트
+        for(int i=0; i<=n; i++){
+            graph.add(new ArrayList<Integer>());
+        }
+        ch = new int[n+1];
+        dis = new int[n+1];
+        for(int i=0; i<m;i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            graph.get(a).add(b);
+        }
+        T.BFS(1);
+        for(int i=2; i<=n;i++){
+            System.out.println(i+" : "+dis[i]);
+        }
+    }
+}
