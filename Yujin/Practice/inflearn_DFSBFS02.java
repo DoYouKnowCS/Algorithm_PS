68e6054b8c543ce542d64126db56d6e246a37539
uujiinn <irislxxyujin@gmail.com>
2024-05-05 16:48:25 +0900

    [success] : inflearn_DFSBFS02 | 최대무게구하기DFS

--- Yujin/Practice/inflearn_DFSBFS02.java
+++ Yujin/Practice/inflearn_DFSBFS02.java
@@ -1,1 +1,28 @@
-//수정2
+package inflearn;
+
+import java.util.Scanner;
+
+public class inflearn_08_02 {
+    static int answer = Integer.MIN_VALUE, c, n;
+    public void DFS(int L, int sum, int[] arr){
+        if(sum>c) return;
+        if(L==n){
+            answer=Math.max(answer,sum);
+        }else{
+            DFS(L+1, sum+arr[L], arr);
+            DFS(L+1, sum, arr);
+        }
+    }
+    public static void main(String[] args) {
+        inflearn_08_02 T = new inflearn_08_02();
+        Scanner sc = new Scanner(System.in);
+        c = sc.nextInt();
+        n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        T.DFS(0,0,arr);
+        System.out.println(answer);
+    }
+}
