f44ae76264f3d12153cd3d4c04de5eb30703f879
uujiinn <irislxxyujin@gmail.com>
2024-05-16 23:57:26 +0900

    [success] : inflearn_DFSBFS09.java | DFS 조합 구하기

--- Yujin/Practice/inflearn_DFSBFS09.java
+++ Yujin/Practice/inflearn_DFSBFS09.java
@@ -1,1 +1,30 @@
-//  수정중
+package inflearn;
+
+import java.util.Scanner;
+
+public class inflearn_08_09 {
+    static int n, m;
+    static int[] combi;
+  
+    public void DFS(int L, int s){
+        if(L==m){
+            for(int x: combi) System.out.print(x+" ");
+        }
+        else{
+            for(int i=s; i<=n; i++){
+                combi[L]= i;
+                DFS(L+1, i+1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        inflearn_08_09 T = new inflearn_08_09();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        combi = new int[m];
+        T.DFS(0,1);
+
+    }
+}
