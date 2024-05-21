ea501c30bc627e5475496161947c271e8b974736
uujiinn <irislxxyujin@gmail.com>
2024-05-22 00:51:59 +0900

    [success] : inflearn_DFSBFS12.java | BFS활용

--- Yujin/Practice/inflearn_DFSBFS12.java
+++ Yujin/Practice/inflearn_DFSBFS12.java
@@ -1,1 +1,67 @@
-//
+package inflearn;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+class Point{
+    public int x, y;
+    Point(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+}
+
+public class inflearn_08_12 {
+    static int[] dx={-1,0,1,0};
+    static int[] dy={0,1,0,-1};
+    static int[][] board, dis;
+    static int n, m;
+    static Queue<Point> Q = new LinkedList<>();
+
+    public void BFS(){
+        while(!Q.isEmpty()) {
+            Point tmp = Q.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = tmp.x + dx[i];
+                int ny = tmp.y + dy[i];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 0) {
+                    board[nx][ny] = 1;
+                    Q.offer(new Point(nx, ny));
+                    dis[nx][ny] = dis[tmp.x][tmp.y] + 1;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        inflearn_08_12 T = new inflearn_08_12();
+        Scanner sc = new Scanner(System.in);
+        m = sc.nextInt();
+        n = sc.nextInt();
+        board = new int[n][m];
+        dis = new int[n][m];
+        for(int i=0; i<n;i++){
+            for(int j=0;j<m;j++){
+                board[i][j]=sc.nextInt();
+                if(board[i][j]==1) Q.offer(new Point(i,j));
+            }
+        }
+        T.BFS();
+        boolean flag = true;
+        int answer = Integer.MIN_VALUE;
+        for(int i=0; i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]==0) flag=false;
+            }
+        }
+        if(flag){
+            for(int i=0; i<n; i++){
+                for(int j=0; j<m; j++){
+                    answer=Math.max(answer, dis[i][j]);
+                }
+            }
+            System.out.println(answer);
+        }
+        else System.out.println(-1);
+    }
+}
