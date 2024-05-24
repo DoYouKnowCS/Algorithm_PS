e756a110bd52bd15cf83f112df4cae245aa3f2fa
uujiinn <irislxxyujin@gmail.com>
2024-05-24 17:29:35 +0900

    [success] : inflearn_Greedy01.java | 씨름선수선발

---
+++ Yujin/Practice/inflearn_Greedy01.java
@@ -0,0 +1,45 @@
+package inflearn;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+class Body implements  Comparable<Body>{
+    public int h, w;
+    Body(int h, int w){
+        this.h = h;
+        this.w = w;
+    }
+    @Override
+    public int compareTo(Body o){
+        return o.h-this.h; //내림차순 기준 h
+    }
+}
+
+public class inflearn_09_01 {
+    public int solution(ArrayList<Body> arr, int n){
+        int cnt=0;
+        Collections.sort(arr);
+        int max = Integer.MIN_VALUE;
+        for(Body ob : arr){
+            if(ob.w>max){
+                max=ob.w;
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+    public static void main(String[] args) {
+        inflearn_09_01 T = new inflearn_09_01();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Body> arr = new ArrayList<>();
+        for(int i=0; i<n;i++){
+            int h = sc.nextInt();
+            int w = sc.nextInt();
+            arr.add(new Body(h,w));
+        }
+        System.out.println(T.solution(arr,n));
+
+    }
+}
