f3a45c1e5d8cb503398db7d69876c9c8ee7bb620
uujiinn <irislxxyujin@gmail.com>
2024-06-26 23:48:08 +0900

    [fail] : inflearn_Greedy08.java | 프림, 최소스패닝트리

--- Yujin/Practice/inflearn_Greedy08.java
+++ Yujin/Practice/inflearn_Greedy08.java
@@ -1,2 +1,53 @@
+package inflearn;
 
-수정중
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+class Edge2 implements Comparable<Edge2>{
+    public int vex;
+    public int cost;
+    Edge2(int vex, int cost){
+        this.vex=vex;
+        this.cost=cost;
+    }
+    @Override
+    public int compareTo(Edge2 ob){
+        return this.cost-ob.cost;
+    }
+}
+
+public class inflearn_09_08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        ArrayList<ArrayList<Edge2>> graph = new ArrayList<ArrayList<Edge2>>();
+        for(int i=0;i<=n;i++){
+            graph.add(new ArrayList<Edge2>());
+        }
+        int[] ch = new int[n+1];
+        for(int i=0;i<m;i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            graph.get(a).add(new Edge2(b,c));
+            graph.get(b).add(new Edge2(a,c));
+        }
+        int answer=0;
+        PriorityQueue<Edge2> pQ = new PriorityQueue<>();
+        pQ.offer(new Edge2(1,0));
+        while(!pQ.isEmpty()){
+            Edge2 tmp = pQ.poll();
+            int ev = tmp.vex;
+            if(ch[ev]==0){
+                ch[ev]=1;
+                answer +=tmp.cost;
+                for(Edge2 ob : graph.get(ev)){
+                    if(ch[ob.vex]==0) pQ.offer(new Edge2(ob.vex, ob.cost));
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
