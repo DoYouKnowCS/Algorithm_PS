5d412b2a0382ecbaab2a4e1d36b181ffe51aa9e4
uujiinn <irislxxyujin@gmail.com>
2024-07-08 23:44:43 +0900

    [success] : inflearn_DP04.java | 탑쌓기

--- Yujin/Practice/inflearn_DP04.java
+++ Yujin/Practice/inflearn_DP04.java
@@ -1,20 +1,39 @@
 package inflearn;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
+
+class Brick implements Comparable<Brick>{
+    public int s,h,w;
+    Brick(int s, int h, int w){
+        this.s=s;
+        this.h=h;
+        this.w=w;
+    }
+
+    @Override
+    public int compareTo(Brick o){
+        return o.s-this.s; //s에 대해 내림차순
+    }
+}
 
 public class inflearn_10_04 {
     static int[] dy;
 
-    public int solution(int[] arr){
+    public int solution(ArrayList<Brick> arr){
         int answer=0;
-        dy = new int[arr.length];
-        dy[0]=1;
-        for(int i=1;i<arr.length;i++){
-            int max=0;
+        Collections.sort(arr);
+        dy[0]=arr.get(0).h;
+        answer=dy[0];
+        for(int i=1;i<arr.size();i++){
+            int max_h=0;
             for(int j=i-1;j>=0;j--){
-                if(arr[j]<arr[i] && dy[j]>max) max=dy[j];
+                if(arr.get(j).w>arr.get(i).w && dy[j]>max_h){
+                    max_h = dy[j];
+                }
             }
-            dy[i] = max+1;
+            dy[i] = max_h+arr.get(i).h;
             answer=Math.max(answer,dy[i]);
         }
 
@@ -25,9 +44,13 @@ public class inflearn_10_04 {
         inflearn_10_04 T = new inflearn_10_04();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[n];
+        ArrayList<Brick> arr = new ArrayList<>();
+        dy = new int[n];
         for(int i=0;i<n;i++) {
-            arr[i] = sc.nextInt();
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            arr.add(new Brick(a,b,c));
         }
         System.out.print(T.solution(arr));
 
